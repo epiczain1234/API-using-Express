1c6013baac67c281bfe907d8165c8111ac04c9f1
epiczain1234 <mukatizain@gmail.com>
2021-03-12 14:34:01 -0500

    Added courses and get routes

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const express = require('express'); // returns functions
 const app = express(); // represents app created from functions
                        // can access endpoints like this
                         // get takes url
+const courses = [
+    {id: 1, name:'course1'},
+    {id: 2, name:'course2'},
+    {id: 3, name:'course3'},
+];
 
 // defining get route, Hello world is the response
 app.get('/', (req, res) => {
@@ -10,7 +15,19 @@ app.get('/', (req, res) => {
 });
 
 app.get('/api/courses', (req, res) => {
-    res.send("black beets in city");
+    res.send(courses);
+});
+
+// : represnts parameters -> requires parameters'
+// ? optional query parameter -> stored in objects with 
+app.get('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id == parseInt(req.params.id));
+    if (!course){
+        res.status(404).send('Course with given ID was not found ')
+    }
+    else{
+        res.send(course);
+    }
 });
 
 // port must be hosted dynamically -> port 3000 may not be available
